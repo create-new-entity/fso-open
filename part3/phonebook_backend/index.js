2d9499adbf6fe4eac99320b53aa68a7232adc049
Pavel <imran.pavel@gmail.com>
2025-08-23 22:12:23 +0600

    3.7

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,10 +1,12 @@
 let { persons } = require('./data')
 
 const express = require('express')
+const morgan = require('morgan')
 const { getRandomInteger } = require('./utils')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 const PORT = 3001
 
