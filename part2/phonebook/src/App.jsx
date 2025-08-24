81d94bad12b674694fd400642487e788547b3882
Pavel <imran.pavel@gmail.com>
2025-08-24 12:43:22 +0600

    3.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -102,7 +102,7 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3001/persons')
+        const response = await axios.get('http://localhost:3002/api/persons')
         setPersons(response.data)
       }
       catch(e) {
