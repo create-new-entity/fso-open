bd6c3863548569317d9638b880d8454f3958fd50
Pavel <imran.pavel@gmail.com>
2025-09-14 18:18:26 +0300

    5.7

--- Part 5 Testing React apps/bloglist-frontend/src/components/Blog.jsx
+++ Part 5 Testing React apps/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const handleVisibility = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const handleLike = () => {
+    consnole.log('Handle like.')
+  }
+
+  const blogStyle = {
+    padding: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: '5px'
+  }
+
+  const visiblityButtonStyle = {
+    marginLeft: '10px'
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button style={visiblityButtonStyle} onClick={handleVisibility}>{showDetails ? 'Hide' : 'View'}</button>
+      </div>
+      {
+        showDetails &&
+        <div>
+          <p>{blog.author}</p>
+          <p>{blog.url}</p>
+          <div>
+            {blog.likes} <button onClick={handleLike}>Like</button>
+          </div>
+        </div>
+      }
+    </div>  
+  )
+}
 
 export default Blog
