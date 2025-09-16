c7dabe9fc1ee235a7056a6527d24e658822a1ad8
Pavel <imran.pavel@gmail.com>
2025-09-17 02:16:10 +0300

    refactored

--- Part 5 Testing React apps/e2e-tests/tests/blog.spec.js
+++ Part 5 Testing React apps/e2e-tests/tests/blog.spec.js
@@ -79,20 +79,7 @@ describe('Blog app', () => {
             })
 
             test('A new blog can be created', async ({ page }) => {
-                const createNewBlogButton = page.getByText('Create New Blog')
-                await expect(createNewBlogButton).toBeVisible()
-                await createNewBlogButton.click()
-
-                const titleLocator = page.getByLabel('title:')
-                const authorLocator = page.getByLabel('author:')
-                const urlLocator = page.getByText('url:')
-                const createButton = page.getByText('Create', { exact: true })
-
-                await titleLocator.fill(testBlog.title)
-                await authorLocator.fill(testBlog.author)
-                await urlLocator.fill(testBlog.url)
-                await createButton.click()
-
+                await testUtils.createABlog(page, testBlog)
                 const createdBlog = page.locator('.blog').nth(0)
                 await expect(createdBlog).toBeVisible()
                 const blogTitle = createdBlog.locator('.blog-title')
