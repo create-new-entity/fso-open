7b2ff2ee38d3c7a0d5366200f62b4a43ae585210
Pavel <imran.pavel@gmail.com>
2025-09-27 02:13:58 +0300

    8.9

--- Part 8 GraphQL/library-frontend/src/components/Books.jsx
+++ Part 8 GraphQL/library-frontend/src/components/Books.jsx
@@ -1,9 +1,18 @@
+import { useQuery } from "@apollo/client"
+import { ALL_BOOKS } from "../queries"
+
 const Books = (props) => {
-  if (!props.show) {
-    return null
+  const result = useQuery(ALL_BOOKS)
+
+  if(result.loading) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
   }
 
-  const books = []
+  const books = result.data.allBooks
 
   return (
     <div>
