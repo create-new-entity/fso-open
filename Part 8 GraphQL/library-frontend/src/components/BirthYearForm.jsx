810973b4b75bc60711191e03c75fbe3c46900406
Pavel <imran.pavel@gmail.com>
2025-09-27 03:48:59 +0300

    8.12

--- Part 8 GraphQL/library-frontend/src/components/BirthYearForm.jsx
+++ Part 8 GraphQL/library-frontend/src/components/BirthYearForm.jsx
@@ -1,21 +1,36 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { ALL_AUTHORS, SET_BIRTH_YEAR } from "../queries"
-import { useMutation } from "@apollo/client"
+import { useMutation, useQuery } from "@apollo/client"
 
 const BirthYearForm = () => {
     const [name, setName] = useState('')
     const [born, setBorn] = useState('')
+    const [authors, setAuthors] = useState([])
     const [ changeBirthYear ] = useMutation(SET_BIRTH_YEAR, {
         refetchQueries: [{ query: ALL_AUTHORS }]
     })
 
+    const result = useQuery(ALL_AUTHORS)
+
+    useEffect(() => {
+        if(!result.loading) {
+            setAuthors(result.data.allAuthors)
+            setName(result.data.allAuthors[0].name)
+        }
+    }, [result.data, result.loading])
+
+    if(result.loading) {
+        return (
+            <div>
+            Loading...
+            </div>
+        )
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault()
-
         const variables = { name, setBornTo: parseInt(born, 10) }
         changeBirthYear({ variables })
-
-        setName('')
         setBorn('')
     }
 
@@ -24,10 +39,15 @@ const BirthYearForm = () => {
             <h2>Set Birth Year</h2>
             <form style={{ marginTop: '10px' }} onSubmit={handleSubmit}>
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                    <label>
-                        Name
-                        <input value={name} onChange={(e) => setName(e.target.value)}/>
-                    </label>
+                    <select value={name} onChange={(e) => setName(e.target.value)}>
+                        {
+                            authors.map((author, index) => {
+                                return (
+                                    <option value={author.name} key={index}>{author.name}</option>
+                                )
+                            })
+                        }
+                    </select>
                     <label>
                         Born
                         <input value={born} onChange={(e) => setBorn(e.target.value)}/>
