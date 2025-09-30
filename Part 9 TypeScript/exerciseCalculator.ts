a115328ba078f5ba6b402353e61855d14c35af0e
Pavel <imran.pavel@gmail.com>
2025-09-30 17:37:39 +0300

    9.2

---
+++ Part 9 TypeScript/exerciseCalculator.ts
@@ -0,0 +1,55 @@
+
+
+
+
+function getRating(average: number, target: number): Pick<Result, 'rating' | 'ratingDescription'> {
+    if(average > target) {
+        return {
+            rating: 3,
+            ratingDescription: 'Expectation exceeded. Great job!'
+        }
+    }
+    if(average === target) {
+        return {
+            rating: 2,
+            ratingDescription: 'not too bad but could be better'
+        }
+    }
+    return {
+        rating: 1,
+        ratingDescription: 'Needs improvement.'
+    }
+}
+
+
+interface Result { 
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+function calculateExercises(exerciseHours: number[], target: number): Result {
+    const trainingDays = exerciseHours.filter(h => h > 0).length
+    const totalWorkedOutHours = exerciseHours.reduce((acc, curr) => acc + curr)
+    const averageWorkedOutHourse = totalWorkedOutHours / 7
+
+    const success = averageWorkedOutHourse >= target
+    const { rating, ratingDescription } = getRating(averageWorkedOutHourse, target)
+
+    return {
+        periodLength: exerciseHours.length,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average: averageWorkedOutHourse
+    }
+}
+
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
